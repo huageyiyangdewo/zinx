90aa075d246cd4c6ba5416c721479a2de681c60d
huageyiyangdewo <1981097256@qq.com>
2021-11-03 17:55:14 +0800

    feat： 链接模块方法的实现

--- znet/connection.go
+++ znet/connection.go
@@ -1,6 +1,7 @@
 package znet
 
 import (
+	"fmt"
 	"net"
 	"zinx/ziface"
 )
@@ -31,4 +32,80 @@ func NewConnection(conn *net.TCPConn, connID uint32, callbackApi ziface.HandleFu
 		ExitBuffChan: make(chan bool, 1),
 	}
 	return c
+}
+
+// StartReader 处理 conn 读取数据的Goroutine
+func (c *Connection) StartReader()  {
+	fmt.Println("Reader Goroutine is running...")
+	defer fmt.Println(c.RemoteAddr().String(), " conn reader exit")
+	defer c.Stop()
+
+	for {
+		// 读取数据到buf中
+		buf := make([]byte, 512)
+		cnt, err := c.Conn.Read(buf)
+		if err != nil {
+			fmt.Println("recv buf err: ", err)
+			continue
+		}
+
+
+		// 调用当前链接业务，执行 handle 方法
+		if err = c.handleAPI(c.Conn, buf, cnt); err != nil {
+			fmt.Println("connID ", c.ConnID, " handle is error ", err)
+			c.ExitBuffChan <- true
+			break
+		}
+	}
+}
+
+
+// Start 启动连接，让当前连接工作
+func (c *Connection) Start() {
+
+	// 开启处理该链接读取到客户端数据之后的请求业务
+	go c.StartReader()
+
+	for  {
+		select {
+		case <- c.ExitBuffChan:
+			// 得到退出消息，不在阻塞
+			break
+		}
+	}
+
+}
+
+// Stop 停止连接，结束当前连接
+func (c *Connection) Stop() {
+	// 如果当前链接已经关闭
+	if c.IsClosed == true {
+		return
+	}
+	// todo Connection Stop() 如果用户注册了该链接的关闭回调业务，那么在此刻应该显示调用
+
+	c.IsClosed = true
+	// 关闭socket 链接
+	c.Conn.Close()
+	// 通知从缓冲队列读取数据的业务，该链接已经关闭
+	c.ExitBuffChan <- true
+
+	// 关闭该链接的全部管道
+	close(c.ExitBuffChan)
+
+}
+
+// GetTCPConnection 从当前连接获取原始的socket TCPConn
+func (c *Connection) GetTCPConnection() *net.TCPConn {
+	return c.Conn
+}
+
+// GetConnID 获取当前连接id
+func (c *Connection) GetConnID() uint32 {
+	return c.ConnID
+}
+
+// RemoteAddr 获取远程客户端地址信息
+func (c *Connection) RemoteAddr() net.Addr {
+	return c.RemoteAddr()
 }

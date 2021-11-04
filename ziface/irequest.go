8b4a91612d33eaaa8602d5b40d58187af723109d
huageyiyangdewo <1981097256@qq.com>
2021-11-04 10:55:07 +0800

    feat: request 请求封装

---
+++ ziface/irequest.go
@@ -0,0 +1,13 @@
+package ziface
+
+/*
+	IRequest：
+	实际上是把请求的链接信息和请求的数据 包装到了 Request 里
+ */
+type IRequest interface {
+	// GetConnection 获取请求的链接信息
+	GetConnection()  IConnection
+
+	// GetData 获取请求的数据
+	GetData()  []byte
+}
---
+++ znet/request.go
@@ -0,0 +1,22 @@
+package znet
+
+import "zinx/ziface"
+
+type Request struct {
+	// 已经和客户端建立好的链接
+	conn ziface.IConnection
+
+	// 客户端请求的数据
+	data []byte
+}
+
+
+// GetConnection 获取请求的链接信息
+func (r *Request) GetConnection()  ziface.IConnection {
+	return r.conn
+}
+
+// GetData 获取请求的数据
+func (r *Request)  GetData()  []byte  {
+	return r.data
+}
